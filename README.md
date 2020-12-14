23e0247823d5171ab7e1b057df4b1245965a6152
lim web <limweb@hotmail.com>
2020-12-14 15:02:30 +0700

    fixed crtl+m and add v0.0.3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # servit-snippet 
 
     snippet สำหรับ ใช้ ServIt 
+
+    code --install-extension  servit-snippet-0.0.3.vsix 
--- src/extension.js
+++ src/extension.js
@@ -2,7 +2,7 @@ const vscode = require('vscode');
 function activate(context) {
 	console.log('Congratulations, your extension "tlen-ext" is now active!');
 	let disposable = vscode.commands.registerCommand('tlen-ext.tlenVersion', function() {
-		vscode.window.showInformationMessage('Tlen version: 0.0.2');
+		vscode.window.showInformationMessage('Tlen version: 0.0.3');
 	});
 	context.subscriptions.push(disposable);
 }
