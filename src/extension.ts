ec7f833d53262d8a916fe1ca2c780cc9dfe611e7
limweb@hotmail.com <limweb@hotmail.com>
2024-09-05 21:23:48 +0700

    i18n ally v.0.0.34

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,29 @@ export function activate(context: vscode.ExtensionContext): void {
   )
 
   context.subscriptions.push(
+    vscode.commands.registerCommand('tlen-ext.i18nally', async function () {
+      console.log('tlen i18nally--->');
+      let msext: any = vscode.extensions.getExtension("hancel.google-translate");
+      if (!msext?.isActive) {
+        vscode.window.showInformationMessage('Tlen i18n-ally ang Google Translate not Active');
+      } else {
+        let text: string = await vscode.commands.executeCommand("i18n-ally.extract-text")
+        if (text) {
+          console.log('text--->', text);
+          vscode.window.showInformationMessage(text);
+        } else {
+          await vscode.commands.executeCommand("translates.clipboard")
+          let clipboard_content = await vscode.env.clipboard.readText();
+          if ((typeof clipboard_content) === 'string') {
+            let newtext = clipboard_content.toLowerCase().replaceAll(/\s(.)/gm, (x: string) => x.toUpperCase()).replaceAll(' ', '').replaceAll(/[!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/g, '')
+            vscode.env.clipboard.writeText('txt.' + newtext)
+          }
+        }
+      }
+    })
+  )
+
+  context.subscriptions.push(
     vscode.commands.registerCommand('tlen-ext.toogleBookmarks', () => {
       console.log('toogleBookmarks--->');
       bookmarksManager.toggleBookmarks(context)
